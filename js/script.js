1d108e5358cbf2899e0fd73e070081c49ad92bd1
Thanh-1998 <stcorndev@gmail.com>
2022-08-12 17:21:41 +0700

    create folder crm

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const body = document.body;
-const menuItems = document.querySelectorAll('.item-menu');
+const menuItems = document.querySelectorAll('.show-items');
 const listMenu = document.querySelector('.list-menu');
 const toggleMenu = document.querySelector('.arrow-menu');
 const navMenu = document.querySelector('.header-menu');
 const overlay = document.querySelector('.overlay');
+const permissionItems = document.querySelectorAll('.permission-items');
+const seePres = document.querySelectorAll('.see-pre');
 
 
 for (let item of menuItems) {
@@ -17,6 +19,25 @@ for (let item of menuItems) {
 	});
 }
 
+
+for (let itemPre of seePres) {
+	itemPre.addEventListener('click', () => {
+		seePres.forEach((seePre) => {
+			itemPre.parentNode.parentNode.classList.toggle("active");
+		});
+	});
+}
+
+for (let item of menuItems) {
+	item.addEventListener('click', () => {
+		menuItems.forEach((menuItem) => {
+			if (menuItem.classList.contains("active") && menuItem != item) {
+				menuItem.classList.remove("active");
+			}
+		});
+		item.classList.toggle("active");
+	});
+}
 
 let checkMenu = false;
 toggleMenu.addEventListener('click', () => {
